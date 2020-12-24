8c9cf82cdd9059f30d64cbfca53bff2126db30fd
Cocowwy <514658459@qq.com>
2020-12-24 23:31:17 +0800

    修改项目结构

--- orange-api/src/main/java/cn/cocowwy/orange/OrangeApplication.java
+++ orange-api/src/main/java/cn/cocowwy/orange/OrangeApplication.java
@@ -1,16 +1,11 @@
 package cn.cocowwy.orange;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"cn.cocowwy.orange.mapper"})// 扫描mapper所在位置
